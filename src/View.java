79d9918289dc970567a1e08155a31de6ed13da1b
horken7 <horken7@me.com>
2016-09-22 18:20:17 +0100

    Updated the entire View to work more efficiently

--- src/View.java
+++ src/View.java
@@ -1,41 +1,40 @@
 import java.awt.event.*;
 import javax.swing.*;
 
-public class View {
+public class View{
+	private JFrame frame;
+	private JPanel panel;
+	private JLabel label;
+	private JButton button;
+	
 	public View(){
+		this.frame = new JFrame();
+		this.panel = new JPanel();
+		this.label = new JLabel();
+		this.button = new JButton();		
 		initView();
 	}
 	
 	public void initView(){
-	    JFrame f = new JFrame("Text Field Examples");
-	    f.setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
-        f.setTitle("Password generator");
-	    
-	    JPanel p = new JPanel();
-	    BoxLayout boxlayout = new BoxLayout(p, BoxLayout.Y_AXIS);
-	    p.setLayout(boxlayout);
-        
-        JTextField textField = new javax.swing.JTextField();
-        textField.setHorizontalAlignment(javax.swing.JTextField.RIGHT);
-        textField.setEditable(false);
-        textField.setFont(new java.awt.Font("Tahoma", 0, 24));
-        p.add(textField);
-        
-        JButton button = new javax.swing.JButton();
-        button.setFont(new java.awt.Font("Tahoma", 0, 24));
-        button.setText("Generate password");
-        button.setMargin(new java.awt.Insets(2, 0, 2, 0));
-        button.addActionListener(new ActionListener() {
+		panel.setLayout(new BoxLayout(panel, BoxLayout.PAGE_AXIS));
+		button.setText("Press me");
+		button.addActionListener(new ActionListener() {
         	public void actionPerformed(ActionEvent e) {
-        		// TODO: implement action performed
+        		updateView("Updated!");
         	}
         });
-        
-        p.add(button);
-	    
-        f.add(p);
-	    f.pack();
-	    f.setVisible(true);
+		updateView("This is a label!");
+		frame.setSize(300,300);
+		frame.setLocationRelativeTo(null);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.setVisible(true);
+		
 	}
-
+	
+	public void updateView(String s){
+		label.setText(s);
+		panel.add(label);
+		panel.add(button);
+		frame.add(panel);
+	}	
 }
